8856b6896c44d19c2b9675874e1d9b6893b8e2ac
Phillip Malboeuf <phil@phils.computer>
2023-10-24 09:48:06 -0400

    header/footer

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,14 @@
 console.log('Yardsale')
+
+const epigraph = document.getElementById("epigraphConfiguratorLite");
+if (epigraph) {
+  window.epigraph = epigraph
+  window.addEventListener(
+    "core:ready",
+    (event) => {
+      // Here you could execute tasks like, updating product pricing, populating a custom UI if need be.
+      console.log("Processes that need to happend once the core is ready");
+      console.log(event)
+    }
+  );
+}

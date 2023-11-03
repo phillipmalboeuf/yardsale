8eb7edcffa91e5b80efb704345b1d7ffd6cef36c
Phillip Malboeuf <phil@phils.computer>
2023-11-03 17:40:28 -0400

    getting there

--- assets/script.js
+++ assets/script.js
@@ -9,3 +9,34 @@ if (epigraph) {
     }
   );
 }
+
+const builder = document.getElementById("pole-builder");
+if (builder) {
+  builder.addEventListener("submit", e => {
+    // e.preventDefault()
+
+    const formData = new FormData(e.target);
+    const formProps = Object.fromEntries(formData);
+
+    console.log(formProps)
+  })
+}
+
+const variants = {
+  'Gondola': 'red',
+  'Powder': 'sand',
+  'Glade': 'green',
+  'Sunlight': 'yellow',
+}
+
+const classes = document.querySelectorAll("input[data-3d-class]");
+if (classes) {
+  classes.forEach(c => {
+    c.addEventListener("click", () => {
+      if (window.epigraph) {
+        window.epigraph.api.makeProductClassIdActive(c.getAttribute("data-3d-class"));
+        window.epigraph.api.switchVariantForProductClass(c.getAttribute("data-3d-class"), variants[c.getAttribute("data-value")])
+      }
+    })
+  })
+}
